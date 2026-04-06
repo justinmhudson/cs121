b9b974aabe45c8a5025fa5b93b9dc41fde6da5f6
Justin Hudson <jhud2010@gmail.com>
2026-04-06 16:42:15 -0700

    fixed tag

--- Assignment_3_4.py
+++ Assignment_3_4.py
@@ -1,5 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-get_ipython().system('jupyter nbconvert --to script --no-prompt --TagRemovePreprocessor.remove_cell_tags="[\'remove\']" Assignment_3_4.ipynb')
+################################
+
+# Student: Justin Hudson
+# Instructor: Kaleab Gorfu
+# CS 121: Python for DS and ML
+# Date: 05/04/2026
+
+################################
+
+import random
+
+def validate(number_string):
+    if number_string.isdecimal() and int(number_string) >= 1 and int(number_string) <= 10:
+        return True
+    else:
+        return False
+
+counter = 0
+random_number = random.randint(1,10)
+
+print("\n")
+while True:
+    while True:
+        user_input = input("Guess any number from 1 to 10: ")
+        if validate(user_input):
+            break
+        print("Invalid Entry. Please enter an integer between 1 and 10 inclusive.\n")
+
+    counter += 1
+    user_guess = int(user_input)
+
+    if user_guess == random_number:
+        print("You got it!")
+        print(f"And it only took {counter} tr{'y' if counter == 1 else 'ies'}!\n")
+        break
+    elif user_guess < random_number:
+        print("Too low!\n")
+    else:
+        print("Too high!\n")
 
