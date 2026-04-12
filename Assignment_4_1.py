8bbec38d3922184752bd5b3af3701f617cc4c7ce
Justin Hudson <jhud2010@gmail.com>
2026-04-12 15:13:46 -0700

    A4

--- Assignment_4_1.py
+++ Assignment_4_1.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+################################
+
+# Student: Justin Hudson
+# Instructor: Kaleab Gorfu
+# CS 121: Python for DS and ML
+# Date: 19/04/2026
+
+################################
+
 import numpy as np
 
 np.linspace(0.0, 5.0, num=15)
+
+
+get_ipython().system('jupyter nbconvert --to script --no-prompt --TagRemovePreprocessor.remove_cell_tags="[\'remove\']" Assignment_4_1.ipynb')
 
--- Assignment_4_3.py
+++ Assignment_4_3.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+################################
+
+# Student: Justin Hudson
+# Instructor: Kaleab Gorfu
+# CS 121: Python for DS and ML
+# Date: 19/04/2026
+
+################################
+
 #   i.
 import random
 get_ipython().run_line_magic('timeit', 'rolls_list = [random.randrange(1, 7) for i in range(0, 6_000_000)]')
